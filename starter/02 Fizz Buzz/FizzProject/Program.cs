1711f0d83de14ec963d34a903bdad4e1123d85f5
rveraromero <rveraromero@microsoft.com>
2024-04-19 14:54:10 -0600

    Fizz Buzz

---
+++ starter/02 Fizz Buzz/FizzProject/Program.cs
@@ -0,0 +1,13 @@
+﻿//Print numbers 1 to 100
+//replace numbers multiple of 3
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
