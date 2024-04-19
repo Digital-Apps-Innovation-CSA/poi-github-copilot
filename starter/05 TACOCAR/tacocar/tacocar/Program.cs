62a937117aedf28429bbe62f993d217ff82704a6
rveraromero <rveraromero@microsoft.com>
2024-04-19 15:09:32 -0600

    tacocar

---
+++ starter/05 TACOCAR/tacocar/tacocar/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//function that give me a paindrome number
+Console.WriteLine(NextPalindrome(30));
+Console.WriteLine(NextPalindrome(1234));
+
+static int NextPalindrome(int n)
+{
+    while (true)
+    {
+        n++;
+        if (IsPalindrome(n.ToString()))
+        {
+            return n;
+        }
+    }
+}
+
+static bool IsPalindrome(string s)
+{
+    int start = 0;
+    int end = s.Length - 1;
+    while (start < end)
+    {
+        if (s[start] != s[end])
+        {
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
+}
