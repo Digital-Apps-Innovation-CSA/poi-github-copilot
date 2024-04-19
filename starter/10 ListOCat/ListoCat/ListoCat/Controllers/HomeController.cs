36ab1fbe072b64f0f2315d15f59a7f701986ed99
rveraromero <rveraromero@microsoft.com>
2024-04-19 15:47:49 -0600

    ListoCat

---
+++ starter/10 ListOCat/ListoCat/ListoCat/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿//call this api https://catfact.ninja/ and return on Index the data
+
+using ListoCat.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace ListoCat.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("https://catfact.ninja/fact");
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    // Assuming the API returns JSON that can be deserialized into a Fact object.
+                    Fact fact = JsonSerializer.Deserialize<Fact>(responseBody);
+
+                    return View(fact);
+                }
+                catch (HttpRequestException e)
+                {
+                    _logger.LogError("Error fetching data from API: {0}", e.Message);
+                    return View();
+                }
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ starter/10 ListOCat/ListoCat/ListoCat/Models/Fact.cs
@@ -0,0 +1,8 @@
+﻿namespace ListoCat.Models
+{
+    public class Fact
+    {
+        public string fact { get; set; }
+        public int Length { get; set; }
+    }
+}
