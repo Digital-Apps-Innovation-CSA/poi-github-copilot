d06798dd8d772fb50028023dbb0cc942bf5dcdc3
rveraromero <rveraromero@microsoft.com>
2024-04-19 14:59:53 -0600

    Fatorial

---
+++ starter/03 The factorial!/Factorial/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//function to calculate factorial
+Console.WriteLine(Factorial(4));
+
+
+static int Factorial(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * Factorial(n - 1);
+    }
+}
