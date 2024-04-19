a33e6c6019439310f729d2fff59722b0a8e4f7d4
rveraromero <rveraromero@microsoft.com>
2024-04-19 15:03:06 -0600

    Palindrome

---
+++ starter/04 TACOCAT/Tacocat/Tacocat/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//Function to validate palindrome string
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine(IsPalindrome("ana"));
+Console.WriteLine(IsPalindrome("sol"));
+
+
+static bool IsPalindrome(string s)
+{
+    int start = 0;
+    int end = s.Length - 1;
+    while (start < end)
+    {
+        if (s[start] != s[end])
+        {
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
+}
